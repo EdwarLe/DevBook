1492addf2aa372dd605433e11989abcfa6b6525a
Edwar León <edwar.leon.m@gmail.com>
2024-06-19 23:36:41 -0500

    Corrección modal notificaciónes estilos

--- js/modal-notifications.js
+++ js/modal-notifications.js
@@ -5,13 +5,18 @@ const notificaciones = document.querySelector(".notificationsList")
 async function dataJSONNotifications() {
     const response = await fetch("../js/utils/db-publicaciones.json");
     const { posts } = await response.json();
-    const listNotifications = notificationsList(posts, notificaciones)
+    // const listNotifications = notificationsList(posts, notificaciones)
 }
 dataJSONNotifications()
 
 const modalNotifications = document.getElementById("modal-notifications")
 const btnNotifications = document.getElementById("notifications")
+const closeNotifications = document.getElementById("close-notifications")
 
 btnNotifications.addEventListener("click", () => {
     modalNotifications.classList.toggle("hidden-notifications")
+})
+
+closeNotifications.addEventListener("click", () => {
+    modalNotifications.classList.add("hidden-notifications")
 })
