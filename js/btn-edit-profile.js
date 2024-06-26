b6e2d18f596f87ee10617a8fbb55fa14249b0e10
Edwar León <edwar.leon.m@gmail.com>
2024-06-26 02:48:40 -0500

    Corrección modal perfil

--- js/btn-edit-profile.js
+++ js/btn-edit-profile.js
@@ -4,6 +4,7 @@ const btn_edit_profile = document.getElementById("btn-edit-profile")
 const container_profile = document.getElementById("container-profile")
 const btn_close_menu_profile = document.getElementById("close-menu-profile")
 const container_details_profile_configuration = document.getElementById("container-details-profile-configuration")
+const containerProfileConfiguration = document.getElementById("container-profile-configuration")
 const details = document.getElementById("details")
 const configuration = document.getElementById("configuration")
 
@@ -11,6 +12,7 @@ const configuration = document.getElementById("configuration")
 // Esta funcion muestra el menu de editar perfil en pantallas moviles 
 btn_edit_profile.addEventListener("click", function () {
     container_profile.classList.toggle("mostrar-menu")
+    containerProfileConfiguration.classList.toggle("mostrar-menu")
     // container_details_profile_configuration.style.display = "grid"
     // details.style.display = "none"
     // configuration.style.display = "none"
@@ -19,6 +21,8 @@ btn_edit_profile.addEventListener("click", function () {
 // Esta funcion cierra el menu de editar perfil en pantallas moviles y en tablet
 btn_close_menu_profile.addEventListener("click", function () {
     container_profile.classList.remove("mostrar-menu")
+    containerProfileConfiguration.classList.remove("mostrar-menu")
+
     // container_details_profile_configuration.style.display = "none"
 })
 
